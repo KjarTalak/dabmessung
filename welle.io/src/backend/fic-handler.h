93dc7c21aa46c4addc152844d120394a32b8dd86
KjarTalak <44828505+KjarTalak@users.noreply.github.com>
2020-12-14 20:56:05 +0100

    Add files via upload

---
+++ welle.io/src/backend/fic-handler.h
@@ -0,0 +1,67 @@
+/*
+ *    Copyright (C) 2018
+ *    Matthias P. Braendli (matthias.braendli@mpb.li)
+ *
+ *    Copyright (C) 2013
+ *    Jan van Katwijk (J.vanKatwijk@gmail.com)
+ *    Lazy Chair Programming
+ *
+ *    This file is part of the SDR-J (JSDR).
+ *    SDR-J is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    SDR-J is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with SDR-J; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+/*
+ *  FIC data
+ */
+#ifndef __FIC_HANDLER
+#define __FIC_HANDLER
+
+#include <mutex>
+#include <cstdio>
+#include <cstdint>
+#include "viterbi.h"
+#include "fib-processor.h"
+#include "radio-controller.h"
+
+class FicHandler: public Viterbi
+{
+    public:
+        FicHandler(RadioControllerInterface& mr);
+        void    processFicBlock(int16_t *data, int16_t blkno);
+        void    setBitsperBlock(int16_t b);
+        void    clearEnsemble();
+        bool    syncReached();
+        int16_t getFicRatio();
+
+        FIBProcessor fibProcessor;
+
+    private:
+        RadioControllerInterface& myRadioInterface;
+        void        processFicInput(int16_t *ficblock, int16_t ficno);
+        const int8_t *PI_15;
+        const int8_t *PI_16;
+        std::vector<uint8_t> bitBuffer_out;
+        std::vector<int16_t> ofdm_input;
+        int16_t     index = 0;
+        int16_t     bitsperBlock = 2 * 1536;
+        int16_t     ficno = 0;
+        int16_t     ficRatio = 0;
+        uint8_t     PRBS[768];
+        uint8_t     shiftRegister[9];
+};
+
+#endif
+
+
---
+++ welle.io/src/backend/viterbi.h
@@ -0,0 +1,78 @@
+#ifndef __VITERBI__
+#define __VITERBI__
+/*
+ *  Viterbi.h according to the SPIRAL project
+ */
+#include    "dab-constants.h"
+#include    "MathHelper.h"
+
+//  For our particular viterbi decoder, we have
+#define RATE    4
+#define NUMSTATES 64
+#define DECISIONTYPE uint32_t
+//#define DECISIONTYPE uint8_t
+//#define DECISIONTYPE_BITSIZE 8
+#define DECISIONTYPE_BITSIZE 32
+#define COMPUTETYPE uint32_t
+
+//decision_t is a BIT vector
+typedef union {
+    DECISIONTYPE t[NUMSTATES/DECISIONTYPE_BITSIZE];
+    uint32_t w[NUMSTATES/32];
+    uint16_t s[NUMSTATES/16];
+    uint8_t c[NUMSTATES/8];
+} decision_t __attribute__ ((aligned (16)));
+
+typedef union {
+    COMPUTETYPE t[NUMSTATES];
+} metric_t __attribute__ ((aligned (16)));
+
+/* State info for instance of Viterbi decoder
+*/
+
+struct v {
+    /* path metric buffer 1 */
+    __attribute__ ((aligned (16))) metric_t metrics1;
+    /* path metric buffer 2 */
+    __attribute__ ((aligned (16))) metric_t metrics2;
+    /* Pointers to path metrics, swapped on every bit */
+    metric_t   *old_metrics,*new_metrics;
+    decision_t *decisions;   /* decisions */
+};
+
+class Viterbi
+{
+    public:
+        Viterbi(int16_t);
+        ~Viterbi(void);
+        Viterbi(const Viterbi& other) = delete;
+        Viterbi& operator=(const Viterbi& other) = delete;
+        void deconvolve(int16_t *input, uint8_t *output);
+
+    private:
+        struct v    vp;
+        COMPUTETYPE Branchtab   [NUMSTATES / 2 * RATE] __attribute__ ((aligned (16)));
+        //  int parityb     (uint8_t);
+        int parity(int x);
+        void partab_init (void);
+        //  uint8_t Partab  [256];
+        void init_viterbi(struct v *, int16_t starting_state);
+
+        void update_viterbi_blk_GENERIC( struct v *vp,
+                                         COMPUTETYPE *syms,
+                                         int16_t nbits);
+
+        void chainback_viterbi( struct v *vp,
+                                uint8_t *data, /* Decoded output data */
+                                int16_t nbits, /* Number of data bits */
+                                uint16_t endstate); /*Terminal encoder state */
+
+        void BFLY( int i, int s, COMPUTETYPE * syms, struct v * vp, decision_t * d);
+
+        uint8_t *data;
+        COMPUTETYPE *symbols;
+        int16_t frameBits;
+};
+
+#endif
+
